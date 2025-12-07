f258d7b7401b66110a15dccfcc59bdb4d5a5de74
B3L4CQU4 <100285551+B3L4CQU4@users.noreply.github.com>
2025-12-07 20:57:03 +0300

    Classes

--- Program.cs
+++ Program.cs
@@ -3,59 +3,143 @@ using System;
 
 
 namespace MyApp {
-    public enum Month {
-        January = 1, February, March, April,
-        May, June, July, August, September,
-        October, November, December
-    }
+
     class Program {
+
+        class Unit {
+            // Имя
+            public string Name { get; }
+
+            // Здоровье
+            private float _health;
+            public float Health => _health; 
+
+            // Урон
+            public int Damage { get; } 
+        
+            // Броня
+            public float Armor { get; } 
+
+            // Конструкторы
+            public Unit() : this("Unknown Unit") {}
+
+            public Unit(string name) {
+                Name = name;
+                Damage = 5;
+                Armor = 0.6f;
+                _health = 100f;
+            }
+
+            // Получение фактического здоровья
+            public float GetRealHealth() {
+                return _health * (1f + Armor);
+            }
+
+            // Получение урона
+            public bool SetDamage(float value)
+            {
+                _health -= value * Armor;
+                return _health <= 0f;
+            }
+        }
+
+        class Weapon {
+            // Имя
+            public string Name { get; }
+
+            // Минимальный урон
+            public int MinDamage { get; private set; }
+
+            // Максимальный урон
+            public int MaxDamage { get; private set; }
+
+            // Прочность
+            private float _Durability;
+
+            // Конструкторы
+            public Weapon(string name) {
+                Name = name;
+                _Durability = 1f;
+                MinDamage = 1;
+                MaxDamage = 10;
+            }
+
+            public Weapon(string name, int minDamage, int maxDamage) : this(name) {
+                SetDamageParams(minDamage, maxDamage);
+            }
+
+            // Установка значений
+            public void SetDamageParams(int minDamage, int maxDamage)
+            {
+                int originalMin = minDamage;
+                int originalMax = maxDamage;
+                
+                // Корректируем отдельные значения
+                if (minDamage < 1)
+                {
+                    minDamage = 1;
+                    Console.WriteLine($"Для оружия '{Name}' выполнена форсированная установка минимального значения урона: 1.");
+                }
+                
+                if (maxDamage <= 1)
+                {
+                    maxDamage = 10;
+                    Console.WriteLine($"Для оружия '{Name}' установлено максимальное значение урона: 10 (т.к. переданное значение {originalMax} <= 1).");
+                }
+                
+                // Проверяем порядок 
+                if (minDamage > maxDamage)
+                {
+                    (minDamage, maxDamage) = (maxDamage, minDamage);
+                    Console.WriteLine($"Некорректные входные данные для оружия '{Name}': " +
+                                    $"minDamage ({originalMin}) > maxDamage ({originalMax}). " +
+                                    $"Значения были поменяны местами.");
+                }
+                
+                MinDamage = minDamage;
+                MaxDamage = maxDamage;
+            }
+            public int GetDamage()
+            {
+                return (MinDamage + MaxDamage) / 2;
+            }
+        }
         static void Main(string[] args) {
-            // Задание 1
-            int[] fibonacciArray = new int[10];
+            // Тесты SetDamageParams
+            /*
+            Console.WriteLine("");
+            Weapon axe = new Weapon("Топор", 20, 10);
+            Console.WriteLine(
+                $"Оружие: {axe.Name}, Min: {axe.MinDamage}, Max: {axe.MaxDamage}, Средний: {axe.GetDamage()}"
+            );
 
-            for (int i = 0; i < fibonacciArray.Length; i++) {
-                fibonacciArray[i] = i switch {
-                    0 => 0,
-                    1 => 1,
-                    _ => fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2]
-                };
+            Console.WriteLine("");
+            Console.WriteLine("");
 
-            }
-            Console.WriteLine("Задание 1");
-            Console.WriteLine(string.Join(", ", fibonacciArray));
+            // Тест с minDamage < 1
+            Weapon dagger = new Weapon("Кинжал", -5, 8);
+            Console.WriteLine(
+                $"Оружие: {dagger.Name}, Min: {dagger.MinDamage}, Max: {dagger.MaxDamage}, Средний: {dagger.GetDamage()}"
+            );
 
-            // Задание 2
-            Console.WriteLine("Задание 2");
-            for (int i = 0; i <= 20; i++) {
-                if (i % 2 == 0 & i != 0) {
-                    Console.Write($"{i} ");
-                }
-            }
-            Console.WriteLine();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            
+            // Тест с maxDamage <= 1
+            Weapon staff = new Weapon("Посох", 3, 1);
+            Console.WriteLine(
+                $"Оружие: {staff.Name}, Min: {staff.MinDamage}, Max: {staff.MaxDamage}, Средний: {staff.GetDamage()}"
+            );
 
-            // Задание 3
-            Console.WriteLine("Задание 3");
-            for (int i = 1; i <= 5; i++) {
-                for (int j = 1; j <= 5; j++) {
-                    Console.Write($"{i} * {j} = {i * j}\t");
-                }
-                Console.WriteLine();
-            }
-
-            // Задание 4
-            Console.WriteLine("Задание 4");
-            string password = "qwerty";
-            string userInput;
-            do {
-                Console.WriteLine("Введите пароль: ");
-                userInput = Console.ReadLine() ?? "";
-                if (userInput != password) {
-                    Console.WriteLine("Неверный пароль");
-                }
-
-            } while (userInput != password);
-            Console.WriteLine("Пароль верный");
-
+            Console.WriteLine("");
+            Console.WriteLine("");
+            
+            // Тест всех проверок сразу
+            Weapon weirdWeapon = new Weapon("Странное оружие", 20, -5);
+            Console.WriteLine(
+                $"Оружие: {weirdWeapon.Name}, Min: {weirdWeapon.MinDamage}, Max: {weirdWeapon.MaxDamage}, Средний: {weirdWeapon.GetDamage()}"
+            );
+            */
         }
     }
 }
